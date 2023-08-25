9e9d67b5b86ee4a7499056a8bb9cde42a6278fac
Ubuntu <ubuntu@ip-172-31-6-67.eu-central-1.compute.internal>
2023-08-25 00:37:01 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static_pages#landing_page"
 
+  get 'static_pages/privacy_policy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "privacy_policy", to: "static_pages#privacy_policy"
   # Defines the root path route ("/")
   # root "articles#index"
 end
